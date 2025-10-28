f3076d817b3dc13da335955e20e5f15db80af5b6
Fai <shinniunndoes@gmail.com>
2025-10-28 15:45:47 +0900

    Create health.js

---
+++ src/routes/health.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { success } from '../utils/response.js';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  success(res, 'Server is healthy');
+});
+
+export default router;
