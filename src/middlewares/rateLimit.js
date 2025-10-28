aab2bebe3f62741e7fa3a7bc441e384d26440e52
Fai <shinniunndoes@gmail.com>
2025-10-28 15:59:50 +0900

    Update rateLimit.js

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,7 +1,13 @@
 import rateLimit from 'express-rate-limit';
+import { CONFIG } from '../config/config.js';
 
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: { success: false, message: 'Too many requests, please try again later.' }
+  windowMs: 10 * 60 * 1000,
+  max: CONFIG.RATE_LIMIT,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: 'Too many requests. Try again later.'
+  }
 });
