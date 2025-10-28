2a84d76742a6b51e4c938cc6b266606020020547
Fai <shinniunndoes@gmail.com>
2025-10-28 15:58:36 +0900

    Update security.js

--- src/middlewares/security.js
+++ src/middlewares/security.js
@@ -1,7 +1,11 @@
 export const securityHeaders = (req, res, next) => {
-  res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('Referrer-Policy', 'no-referrer');
-  res.setHeader('Permissions-Policy', 'geolocation=(), microphone=()');
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; img-src * data:; style-src 'self' 'unsafe-inline'; script-src 'self'"
+  );
   next();
 };
