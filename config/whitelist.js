183fa3ee22862f4c743d06d032d6283d2c94596c
Fai <shinniunndoes@gmail.com>
2025-10-28 15:27:27 +0900

    Create whitelist.js

---
+++ config/whitelist.js
@@ -0,0 +1,8 @@
+// config/whitelist.js
+export const WHITELIST = [
+  "https://example.com",
+  "https://yourapp.vercel.app",
+  "http://localhost:3000",
+  "https://api.openai.com",
+  "https://maps.googleapis.com",
+];
